870471319f09435474e95063c25e817302d502ab
Andy Miles <akmiles@icloud.com>
2024-11-13 18:07:32 -0800

    added bootsrap and an example of how to add a new page

--- cool_counters/counter/models.py
+++ cool_counters/counter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Counter(models.Model):
     key = models.CharField(max_length=10)
-    value = models.IntegerField() 
+    value = models.IntegerField()
--- cool_counters/counter/views.py
+++ cool_counters/counter/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Counter
 
+
 def index(request):
     if len(Counter.objects.filter(key='counter')) == 0:
         counter = Counter(key='counter', value=0)
         counter.save()
     else:
         counter = get_object_or_404(Counter, key='counter')
-    
-    counter.value+=1
+
+    counter.value += 1
     counter.save()
-    context = {'value': counter.value}
+    context = {'value': counter.value, 'sum': 99 * 10}
     return render(request, 'counter/index.html', context)
 
+
+def andy(request):
+    context = {'message': "Hello from Andy"}
+    return render(request, 'counter/andy.html', context)
